2bd138678c52ed5efc4bb3ab7064cde40c462f56
arunav7 <harsharunav9@gmail.com>
2022-11-08 03:13:59 +0530

    Init Commit - Kenshi

---
+++ pages/blog/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div>
+      <p> Post: {id} </p>
+    </div>
+  );
+};
+
+export default Post;
